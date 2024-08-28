98033f921f9bbda6b0fa9790a6466ae81da6eec2
tonoy3125 <shaifshajedt@gmail.com>
2024-08-28 19:37:42 +0600

    folder structure created

---
+++ src/Layout/MainLayout/MainLayout.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/Pages/Shared/Footer/Footer";
+import Navbar from "@/Pages/Shared/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/Routes/Routes.tsx
@@ -0,0 +1,18 @@
+import MainLayout from "@/Layout/MainLayout/MainLayout";
+import Home from "@/Pages/Home/Home";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;
