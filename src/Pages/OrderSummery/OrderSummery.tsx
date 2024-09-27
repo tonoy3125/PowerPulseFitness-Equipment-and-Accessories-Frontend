c010e6ae4a3841cd4979580611125833566bf03b
tonoy3125 <shaifshajedt@gmail.com>
2024-09-27 23:27:05 +0600

    some design chnages commit

--- src/Pages/OrderSummery/OrderSummery.tsx
+++ src/Pages/OrderSummery/OrderSummery.tsx
@@ -5,6 +5,7 @@ import AdsNavbar from "../Shared/AdsNavbar/AdsNavbar";
 import Footer from "../Shared/FooterPage/Footer/Footer";
 import SingleProduct from "../Products/SingleProduct";
 import OrderSummeryBanner from "@/components/OrderSummeryBanner/OrderSummeryBanner";
+import { BsArrowRight } from "react-icons/bs";
 
 const OrderSummery = () => {
   const { id } = useParams();
@@ -27,201 +28,180 @@ const OrderSummery = () => {
       <AdsNavbar />
       <Navbar />
       <OrderSummeryBanner />
-      <div className="mt-10 mb-10 max-w-7xl mx-auto">
-        <div className=" flex items-center justify-center mt-8 pb-10">
-          <ol className="items-center flex w-full max-w-2xl text-center text-xs sm:text-sm font-medium text-gray-500 dark:text-gray-400 md:text-base font-poppins">
-            <li className="after:border-1 flex items-center text-[#1D4ED8] after:mx-1 sm:after:mx-3 semi-sm:after:mx-6 after:hidden after:h-1 after:w-full after:border-b after:border-[#1D4ED8] dark:text-primary-500 dark:after:border-gray-700 sm:after:inline-block sm:after:content-[''] md:w-full xl:after:mx-10">
-              <span className="flex items-center after:mx-2 after:text-gray-200  dark:after:text-gray-500 sm:after:hidden">
-                <svg
-                  className="me-2 h-4 w-4 sm:h-5 sm:w-5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                  />
-                </svg>
-                Cart
-              </span>
-            </li>
-
-            <li className="after:border-1 flex items-center text-[#1D4ED8] after:mx-1 sm:after:mx-3 semi-sm::after:mx-6 after:hidden after:h-1 after:w-full after:border-b after:border-[#1D4ED8] dark:text-primary-500 dark:after:border-gray-700 sm:after:inline-block sm:after:content-[''] md:w-full xl:after:mx-10">
-              <span className="flex items-center after:mx-2 after:text-gray-200  dark:after:text-gray-500 sm:after:hidden">
-                <svg
-                  className="me-2 h-4 w-4 sm:h-5 sm:w-5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                  />
-                </svg>
-                Checkout
-              </span>
-            </li>
-
-            <li className="flex shrink-0 items-center text-[#1D4ED8]">
-              <svg
-                className="me-2 h-4 w-4 sm:h-5 sm:w-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
+      <div className="mt-10 mb-10 lg:max-w-7xl mx-auto">
+        <div className=" flex items-start md:items-center flex-col md:flex-row justify-center gap-5 lg:gap-20 mt-8 pb-24 mx-5 md:mx-0">
+          <div className="flex items-center gap-5 lg:gap-14">
+            <div className="flex items-center gap-2 lg:gap-5">
+              <div className="text-xl font-poppins font-semibold border px-4 py-1 bg-[#EC3D08] text-white">
+                <span>1</span>
+              </div>
+              <h1
+                className="font-oswald uppercase text-base font-medium"
+                style={{ lineHeight: "1", letterSpacing: "0.025em" }}
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                />
-              </svg>
-              Order summary
-            </li>
-          </ol>
-        </div>
-        <h3 className="text-[#7C7C7C] font-poppins font-medium text-lg ">
-          Thank you. Your order has been received.
-        </h3>
-        <div className="flex items-center gap-5 rounded-xl p-5 mt-8 bg-[#FFFFFF]">
-          <div className="border-r-[1px] border-dashed border-[#cfc8d8] pr-10 pl-5">
-            <p
-              className="text-[#7C7C7C] uppercase font-oswald text-base font-medium mb-2"
+                Shopping Cart
+              </h1>
+            </div>
+            <span>
+              <BsArrowRight className="text-2xl font-poppins font-semibold hidden md:block" />
+            </span>
+          </div>
+          <div className="flex items-center gap-5 lg:gap-14">
+            <div className="flex items-center gap-2 lg:gap-5">
+              <div className="text-xl font-poppins font-semibold border px-4 py-1 bg-[#EC3D08] text-white">
+                <span>2</span>
+              </div>
+              <h1
+                className="font-oswald uppercase text-base font-medium"
+                style={{ lineHeight: "1", letterSpacing: "0.025em" }}
+              >
+                Payment & Delivery Options
+              </h1>
+            </div>
+            <span>
+              <BsArrowRight className="text-2xl font-poppins font-semibold hidden md:block" />
+            </span>
+          </div>
+          <div className="flex items-center gap-2 lg:gap-5">
+            <div className="text-xl font-poppins font-semibold border px-4 py-1 bg-[#EC3D08] text-white">
+              <span>3</span>
+            </div>
+            <h1
+              className="font-oswald uppercase text-base font-medium"
               style={{ lineHeight: "1", letterSpacing: "0.025em" }}
             >
-              Order number :
-            </p>
-            <p
-              className="text-[#2C2C2C] font-poppins text-sm font-normal"
-              style={{ lineHeight: "1.5" }}
-            >
-              {singleOrder?.data.orderNumber}
-            </p>
-          </div>
-          <div className="border-r-[1px] border-dashed border-[#cfc8d8] pr-10 pl-5">
-            <p
-              className="text-[#7C7C7C] uppercase font-oswald text-base font-medium mb-2"
-              style={{ lineHeight: "1", letterSpacing: "0.025em" }}
-            >
-              Date:
-            </p>
-            <p
-              className="text-[#2C2C2C] font-poppins text-sm font-normal"
-              style={{ lineHeight: "1.5" }}
-            >
-              {singleOrder?.data.createdAt
-                ? formatDate(singleOrder.data.createdAt)
-                : "N/A"}
-            </p>
-          </div>
-          <div className="border-r-[1px] border-dashed border-[#cfc8d8] pr-10 pl-5">
-            <p
-              className="text-[#7C7C7C] uppercase font-oswald text-base font-medium mb-2"
-              style={{ lineHeight: "1", letterSpacing: "0.025em" }}
-            >
-              Total:
-            </p>
-            <p
-              className="text-[#2C2C2C] font-poppins text-sm font-normal"
-              style={{ lineHeight: "1.5" }}
-            >
-              {singleOrder?.data.total}
-            </p>
-          </div>
-          <div className="pr-10 pl-5">
-            <p
-              className="text-[#7C7C7C] uppercase font-oswald text-base font-medium mb-2"
-              style={{ lineHeight: "1", letterSpacing: "0.025em" }}
-            >
-              Payment Method:
-            </p>
-            <p
-              className="text-[#2C2C2C] font-poppins text-sm font-normal"
-              style={{ lineHeight: "1.5" }}
-            >
-              Cash On Delivery
-            </p>
-          </div>
-        </div>
-        <h3 className="text-[#7C7C7C] font-poppins font-medium text-lg mt-14">
-          Pay with cash upon delivery.
-        </h3>
-        <div>
-          <div className="mt-10 mb-10">
-            <h1 className="font-oswald text-3xl text-black font-medium mb-3 uppercase pb-5 pt-5">
-              Order details
+              Order Received
             </h1>
-            <div className=" bg-[#FFFFFF] p-5 semi-sm:p-10 rounded-xl">
-              <div>
-                {singleOrder?.data?.addToCartProduct?.map((item) => (
-                  <div
-                    key={item._id}
-                    className="flex items-center justify-between border-b-[1px] pb-5 pt-5 border-b-gray-400"
-                  >
-                    <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
-                      {item?.productId.name} × {item?.quantity}
-                    </h1>
-                    <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
-                      ${item?.productId.price}
-                    </h1>
-                  </div>
-                ))}
-              </div>
-              <div className="flex items-center justify-between border-b-[1px] pb-5 pt-5 border-b-gray-400">
-                <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
-                  SubTotal
-                </h1>
-                <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
-                  {singleOrder?.data?.subTotal}
-                </h1>
-              </div>
-              <div className="flex items-center justify-between border-b-[1px] pb-5 pt-5 border-b-gray-400">
-                <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
-                  Tax
-                </h1>
-                <h1 className="font-poppins font-medium text-base">
-                  {singleOrder?.data?.tax}
-                </h1>
-              </div>
-              <div className="flex items-center justify-between border-b-[1px] pb-5 pt-5 border-b-gray-400">
-                <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
-                  Shipping
-                </h1>
-                <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
-                  ${singleOrder?.data?.shipping}
-                </h1>
-              </div>
-              <div className="flex items-center justify-between border-b-[1px] pb-5 pt-5 border-b-gray-400">
-                <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
-                  Payment method:
-                </h1>
-                <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
-                  Cash On Delivery
-                </h1>
-              </div>
-              <div className="flex items-center justify-between  pb-5 pt-5 ">
-                <h1 className="font-poppins font-semibold text-base ">Total</h1>
-                <h1 className="font-poppins font-semibold text-base ">
-                  {singleOrder?.data?.total}
-                </h1>
+          </div>
+        </div>
+        <div className="mx-5 lg:mx-0">
+          <h3 className="text-[#7C7C7C] font-poppins font-medium text-lg ">
+            Thank you. Your order has been received.
+          </h3>
+          <div className="flex items-start md:items-center flex-col md:flex-row gap-5 rounded-xl p-5  mt-8 bg-[#FFFFFF]">
+            <div className="md:border-r-[1px] md:border-dashed md:border-[#cfc8d8] md:pr-10 md:pl-5">
+              <p
+                className="text-[#7C7C7C] uppercase font-oswald text-base font-medium mb-2"
+                style={{ lineHeight: "1", letterSpacing: "0.025em" }}
+              >
+                Order number :
+              </p>
+              <p
+                className="text-[#2C2C2C] font-poppins text-sm font-normal"
+                style={{ lineHeight: "1.5" }}
+              >
+                {singleOrder?.data.orderNumber}
+              </p>
+            </div>
+            <div className="md:border-r-[1px] md:border-dashed md:border-[#cfc8d8] md:pr-10 md:pl-5">
+              <p
+                className="text-[#7C7C7C] uppercase font-oswald text-base font-medium mb-2"
+                style={{ lineHeight: "1", letterSpacing: "0.025em" }}
+              >
+                Date:
+              </p>
+              <p
+                className="text-[#2C2C2C] font-poppins text-sm font-normal"
+                style={{ lineHeight: "1.5" }}
+              >
+                {singleOrder?.data.createdAt
+                  ? formatDate(singleOrder.data.createdAt)
+                  : "N/A"}
+              </p>
+            </div>
+            <div className="md:border-r-[1px] md:border-dashed md:border-[#cfc8d8] md:pr-10 md:pl-5">
+              <p
+                className="text-[#7C7C7C] uppercase font-oswald text-base font-medium mb-2"
+                style={{ lineHeight: "1", letterSpacing: "0.025em" }}
+              >
+                Total:
+              </p>
+              <p
+                className="text-[#2C2C2C] font-poppins text-sm font-normal"
+                style={{ lineHeight: "1.5" }}
+              >
+                {singleOrder?.data.total}
+              </p>
+            </div>
+            <div className="md:pr-10 md:pl-5">
+              <p
+                className="text-[#7C7C7C] uppercase font-oswald text-base font-medium mb-2"
+                style={{ lineHeight: "1", letterSpacing: "0.025em" }}
+              >
+                Payment Method:
+              </p>
+              <p
+                className="text-[#2C2C2C] font-poppins text-sm font-normal"
+                style={{ lineHeight: "1.5" }}
+              >
+                Cash On Delivery
+              </p>
+            </div>
+          </div>
+          <h3 className="text-[#7C7C7C] font-poppins font-medium text-lg mt-14">
+            Pay with cash upon delivery.
+          </h3>
+          <div>
+            <div className="mt-10 mb-10 ">
+              <h1 className="font-oswald text-3xl text-black font-medium mb-3 uppercase pb-5 pt-5">
+                Order details
+              </h1>
+              <div className=" bg-[#FFFFFF] p-5 semi-sm:p-10  rounded-xl">
+                <div>
+                  {singleOrder?.data?.addToCartProduct?.map((item) => (
+                    <div
+                      key={item._id}
+                      className="flex items-center justify-between border-b-[1px] pb-5 pt-5 border-b-gray-400"
+                    >
+                      <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
+                        {item?.productId.name} × {item?.quantity}
+                      </h1>
+                      <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
+                        ${item?.productId.price}
+                      </h1>
+                    </div>
+                  ))}
+                </div>
+                <div className="flex items-center justify-between border-b-[1px] pb-5 pt-5 border-b-gray-400">
+                  <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
+                    SubTotal
+                  </h1>
+                  <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
+                    {singleOrder?.data?.subTotal}
+                  </h1>
+                </div>
+                <div className="flex items-center justify-between border-b-[1px] pb-5 pt-5 border-b-gray-400">
+                  <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
+                    Tax
+                  </h1>
+                  <h1 className="font-poppins font-medium text-base">
+                    {singleOrder?.data?.tax}
+                  </h1>
+                </div>
+                <div className="flex items-center justify-between border-b-[1px] pb-5 pt-5 border-b-gray-400">
+                  <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
+                    Shipping
+                  </h1>
+                  <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
+                    ${singleOrder?.data?.shipping}
+                  </h1>
+                </div>
+                <div className="flex items-center justify-between border-b-[1px] pb-5 pt-5 border-b-gray-400">
+                  <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
+                    Payment method:
+                  </h1>
+                  <h1 className="font-poppins font-semibold text-base text-[#2C2C2C]">
+                    Cash On Delivery
+                  </h1>
+                </div>
+                <div className="flex items-center justify-between  pb-5 pt-5 ">
+                  <h1 className="font-poppins font-semibold text-base ">
+                    Total
+                  </h1>
+                  <h1 className="font-poppins font-semibold text-base ">
+                    {singleOrder?.data?.total}
+                  </h1>
+                </div>
               </div>
             </div>
           </div>
