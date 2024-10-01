8fde5aed8295fbf084d44cc4b2c82494e09f4490
tonoy3125 <shaifshajedt@gmail.com>
2024-10-01 21:56:02 +0600

    what we do section responsive issue solved

--- src/components/WhatWeDo/WhatWeDo.tsx
+++ src/components/WhatWeDo/WhatWeDo.tsx
@@ -9,14 +9,14 @@ const WhatWeDo = () => {
           What we do
         </h3>
         <h1
-          className="font-lora text-5xl text-center w-[1200px] mx-auto uppercase text-[#2C2C2C] font-bold mb-5"
+          className="font-lora text-xl semi-sm:text-2xl md:text-4xl lg:text-5xl text-center  md:w-[700px]  lg:w-[1200px] mx-auto uppercase text-[#2C2C2C] font-bold mb-5"
           style={{ lineHeight: "1.2", letterSpacing: "0.025em" }}
         >
           Our Fitness store is fostering an active lifestyle and offering
           innovative, functional gear
         </h1>
         <h3
-          className="font-poppins text-[#2C2C2C] text-center font-semibold text-lg uppercase mb-1"
+          className="font-poppins text-[#2C2C2C] text-center font-semibold text-base sm:text-lg uppercase mb-1"
           style={{ letterSpacing: "0.050em" }}
         >
           Peter Bowman
